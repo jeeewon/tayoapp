d5338e043c29b819dbe9126a27e5d24d1b2a0d2f
hj-jung <hyoju1998@naver.com>
2021-02-03 02:22:13 +0900

    Create AppDelegate.swift

---
+++ tayotayo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  tayotayo
+//
+//  Created by LeeJiwon on 2021/01/31.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
